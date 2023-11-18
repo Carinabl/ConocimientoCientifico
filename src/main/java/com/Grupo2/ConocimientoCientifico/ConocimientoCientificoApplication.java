8d99fb0366a7e13d20a31d8022fd39c288a092f2
carinal <carinablopez@gmail.com>
2023-11-18 14:16:54 -0300

    Iniciamos proyecto back

---
+++ src/main/java/com/Grupo2/ConocimientoCientifico/ConocimientoCientificoApplication.java
@@ -0,0 +1,13 @@
+package com.Grupo2.ConocimientoCientifico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConocimientoCientificoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConocimientoCientificoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Grupo2/ConocimientoCientifico/ConocimientoCientificoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo2.ConocimientoCientifico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConocimientoCientificoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
